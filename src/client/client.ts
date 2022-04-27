d62d30f676004608e672a0cecd0f5ca1d2e815ff
GINI <dove209@naver.com>
2022-04-27 18:21:07 +0900

    Water-like Distortion Effect Step 1

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,371 +1,117 @@
-import * as THREE from 'three'
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+class WaterTexture {
+  size: number;
+  width: number;
+  height: number;
+  radius: number = 0;
+  canvas: any = null;
+  ctx: any;
+  points: any[];
+  maxAge: number;
+  constructor(options: any) {
+    this.size = 64;
+    this.radius = this.size * 0.1;
+    this.points = [];
+    this.maxAge = 64;
+    this.width = this.height = this.size;
+    if (options.debug) {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+      this.radius = this.width * 0.05;
+    }
 
-let loaded = false;
+    this.initTexture();
+    if (options.debug) document.body.append(this.canvas);
+  }
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xf1f1f1);
-scene.fog = new THREE.Fog(0xf1f1f1, 20, 100);
+  initTexture() {
+    this.canvas = document.createElement('canvas');
+    this.canvas.id = 'WaterTexture';
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.ctx = this.canvas.getContext('2d');
+    this.clear();
+  }
 
-const camera = new THREE.PerspectiveCamera(
-  50,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 5;
-camera.position.x = 0;
+  clear() {
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 
-const canvas = document.querySelector('#c') as HTMLCanvasElement ;
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setPixelRatio(window.devicePixelRatio || 1);
-document.body.appendChild(renderer.domElement)
+  addPoint(point: any) {
+    this.points.push({ x: point.x, y: point.y, age: 0 })
+  }
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
-hemiLight.position.set(0, 50, 0);
-scene.add(hemiLight);
-
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
-dirLight.position.set(-8, 12, 8);
-dirLight.castShadow = true;
-dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
-scene.add(dirLight);
-
-const INITIAL_MTL = new THREE.MeshPhongMaterial({
-  color: 0xf1f1f1,
-  shininess: 10,
-});
-const INITIAL_MAP = [
-  { childID: 'back', mtl: INITIAL_MTL },
-  { childID: 'base', mtl: INITIAL_MTL },
-  { childID: 'cushions', mtl: INITIAL_MTL },
-  { childID: 'legs', mtl: INITIAL_MTL },
-  { childID: 'supports', mtl: INITIAL_MTL },
-];
-
-function initColor(parent: any, type: any, mtl: any) {
-  parent.traverse((o: any) => {
-    if((o as THREE.Mesh).isMesh) {
-      if(o.name.includes(type)) {
-        o.material = mtl;
-        o.nameID = type;
+  update() {
+    this.clear();
+    this.points.forEach((point, i) => {
+      point.age += 1;
+      if (point.age > this.maxAge) {
+        this.points.splice(i, i)
       }
-    }
-  })
-}
-const LOADER: any = document.getElementById('js-loader');
-
-let theModel: any;
-const MODEL_PATH = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/chair.glb";
-
-let loader = new GLTFLoader();
-loader.load(MODEL_PATH, function(gltf) {
-  theModel = gltf.scene;
-  
-  theModel.traverse((o: any) => {
-    if((o as THREE.Mesh).isMesh) {
-      o.castShadow = true;
-      o.receiveShadow = true;
-    }
-  })
-
-  theModel.scale.set(2, 2, 2);
-  theModel.position.y = -1;
-  theModel.rotateY(Math.PI);
-
-  for(let object of INITIAL_MAP) {
-    initColor(theModel, object.childID, object.mtl);
-  }
-
-  scene.add(theModel);
-  LOADER.remove();
-
-},
-function(xhr) {
-  console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-},
-function(err) {
-  console.log(err)
-}
-)
-
-// Floor
-let floorGeometry = new THREE.PlaneBufferGeometry(5000, 5000, 1, 1);
-let floorMaterial = new THREE.MeshPhongMaterial({
-  color: 0xeeeeee,
-  shininess: 0
-});
-let floor = new THREE.Mesh(floorGeometry, floorMaterial);
-floor.rotateX(-Math.PI / 2);
-floor.receiveShadow = true;
-floor.position.y = -1;
-scene.add(floor)
-
-
-// Add controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.maxPolarAngle = Math.PI / 2;
-controls.minPolarAngle = Math.PI / 3;
-controls.enableDamping = true;
-controls.enablePan = false;
-controls.dampingFactor = 0.1;
-controls.autoRotate = false;
-controls.autoRotateSpeed = 0.2;
-
-window.addEventListener('resize', onWindowResize, false);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-const DRAG_NOTICE = document.getElementById('js-drag-notice');
-
-function animate() {
-  render()
-  controls.update();
-  requestAnimationFrame(animate);
-
-  if(theModel != null && loaded == false) {
-    initialRotaion();
-    DRAG_NOTICE?.classList.add('start');
-  }
-}
-
-
-
-function render() {
-  renderer.render(scene, camera)
-}
-
-animate();
-
-let activeOption = 'legs';
-
-const colors = [
-  {
-      texture: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/wood_.jpg',
-      size: [2,2,2],
-      shininess: 60
-  },
-  {
-      texture: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/fabric_.jpg',
-      size: [4, 4, 4],
-      shininess: 0
-  },
-  {
-      texture: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/pattern_.jpg',
-      size: [8, 8, 8],
-      shininess: 10
-  },
-  {
-      texture: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/denim_.jpg',
-      size: [3, 3, 3],
-      shininess: 0
-  },
-  {
-      texture: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/quilt_.jpg',
-      size: [6, 6, 6],
-      shininess: 0
-  },
-  { color: '131417' }, 
-  { color: '374047' },
-  { color: '5f6e78' },
-  { color: '7f8a93' },
-  { color: '97a1a7' },
-  { color: 'acb4b9' },
-  { color: 'DF9998' },
-  { color: '7C6862' },
-  { color: 'A3AB84' },
-  { color: 'D6CCB1' },
-  { color: 'F8D5C4' },
-  { color: 'A3AE99' },
-  { color: 'EFF2F2' },
-  { color: 'B0C5C1' },
-  { color: '8B8C8C' },
-  { color: '565F59' },
-  { color: 'CB304A' },
-  { color: 'FED7C8' },
-  { color: 'C7BDBD' },
-  { color: '3DCBBE' },
-  { color: '264B4F' },
-  { color: '389389' },
-  { color: '85BEAE' },
-  { color: 'F2DABA' },
-  { color: 'F2A97F' },
-  { color: 'D85F52' },
-  { color: 'D92E37' },
-  { color: 'FC9736' },
-  { color: 'F7BD69' },
-  { color: 'A4D09C' },
-  { color: '4C8A67' },
-  { color: '25608A' },
-  { color: '75C8C6' },
-  { color: 'F5E4B7' },
-  { color: 'E69041' },
-  { color: 'E56013' },
-  { color: '11101D' },
-  { color: '630609' },
-  { color: 'C9240E' },
-  { color: 'EC4B17' },
-  { color: '281A1C' },
-  { color: '4F556F' },
-  { color: '64739B' },
-  { color: 'CDBAC7' },
-  { color: '946F43' },
-  { color: '66533C' },
-  { color: '173A2F' },
-  { color: '153944' },
-  { color: '27548D' },
-  { color: '438AAC' }
-]
-const TRAY = document.getElementById('js-tray-slide');
-
-function buildColor(colors: any) {
-  colors.forEach((ele: any, index: number) => {
-    let swatch = document.createElement('div');
-    swatch.classList.add('tray__swatch');
-    if(ele.texture) {
-      swatch.style.backgroundImage = "url(" + ele.texture + ")";  
-    } else {
-      swatch.style.background = '#' + ele.color;
-    }
-    swatch.setAttribute('data-key', String(index));
-    TRAY?.append(swatch);
-  });
-}
-buildColor(colors);
-
-const swatches = document.querySelectorAll(".tray__swatch");
-swatches.forEach(swatch => {
-  swatch.addEventListener('click', selectSwatch)  
-});
-function selectSwatch(e: any) {
-  let color = colors[parseInt(e.target.dataset.key)];
-  let new_mtl;
-
-  if (color.texture) {
-    let txt = new THREE.TextureLoader().load(color.texture);
-    txt.repeat.set( color.size[0], color.size[1]);
-    txt.wrapS = THREE.RepeatWrapping;
-    txt.wrapT = THREE.RepeatWrapping;
-    new_mtl = new THREE.MeshPhongMaterial( {
-      map: txt,
-      shininess: color.shininess ? color.shininess : 10
-    });    
-  } else {
-    new_mtl = new THREE.MeshPhongMaterial({
-      color: parseInt('0x' + color.color ),
-      shininess: color.shininess ? color?.shininess : 10
+    })
+    this.points.forEach(point => {
+      this.drawPoint(point);
     })
   }
-  setMaterial(theModel, activeOption, new_mtl);
-};
 
-function setMaterial(parent: any, type: string, mtl:THREE.Material) {
-  parent.traverse((o: any) => {
-    if(o.isMesh && o.nameID != null) {
-      if(o.nameID == type) {
-        o.material = mtl;
-      }
+  drawPoint(point: any) {
+    // Convert normalized position into canvas coordinates
+    let pos = {
+      x: point.x * this.width,
+      y: point.y * this.height
     }
-  })
-}
+    const radius = this.radius;
+    const ctx = this.ctx;
 
+    // Lower the opacity at it gets older
+    let intensity = 1;
+    intensity = 1 - point.age / this.maxAge;
 
-// Select Option
-const options = document.querySelectorAll('.option');
-options.forEach(option => {
-    option.addEventListener('click', selectOption);
-});
+    let color = '255, 255, 255';
 
-function selectOption(e: any) {
-  let option = e.target;
-  activeOption = e.target.dataset.option;
-  options.forEach(otherOption => {
-    otherOption.classList.remove('--is-active');
-  });
-  option.classList.add('--is-active')
-}
+    let offset = this.width * 5;
+    ctx.shadowOffsetX = offset;
+    ctx.shadowOffsetY = offset;
+    ctx.shadowBlur = radius;
+    ctx.shadowColor = `rgba(${color}, ${0.2 * intensity})`;
 
-
-// Opening rotate
-let initRotate = 0;
-function initialRotaion() {
-  initRotate++;
-  if(initRotate <= 120) {
-    theModel.rotateY(Math.PI / 60);
-  } else {
-    loaded = true
+    this.ctx.beginPath();
+    this.ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    this.ctx.arc(pos.x - offset, pos.y - offset, radius, 0, Math.PI * 2);
+    this.ctx.fill();
   }
 }
 
-let slider: any = document.getElementById('js-tray'), sliderItems: any = document.getElementById('js-tray-slide'), difference: any;
-
-function slide(wrapper: any, items: any) {
-  let posX1 = 0,
-      posX2 = 0,
-      posInitial: any,
-      threshold = 20,
-      posFinal,
-      slides = items.getElementsByClassName('tray__swatch');
-  
-  // Mouse events
-  items.onmousedown = dragStart;
-  
-  // Touch events
-  items.addEventListener('touchstart', dragStart);
-  items.addEventListener('touchend', dragEnd);
-  items.addEventListener('touchmove', dragAction);
 
 
-  function dragStart (e: any) {
-    e = e || window.event;
-     posInitial = items.offsetLeft;
-     difference = sliderItems.offsetWidth - slider.offsetWidth;
-     difference = difference * -1;
-    
-    if (e.type == 'touchstart') {
-      posX1 = e.touches[0].clientX;
-    } else {
-      posX1 = e.clientX;
-      document.onmouseup = dragEnd;
-      document.onmousemove = dragAction;
-    }
+
+class App {
+  waterTexture: any;
+  constructor() {
+    this.waterTexture = new WaterTexture({ debug: true })
+    this.tick = this.tick.bind(this);
+    this.init();
   }
 
-  function dragAction (e: any) {
-    e = e || window.event;
-    
-    if (e.type == 'touchmove') {
-      posX2 = posX1 - e.touches[0].clientX;
-      posX1 = e.touches[0].clientX;
-    } else {
-      posX2 = posX1 - e.clientX;
-      posX1 = e.clientX;
-    }
-    
-    if (items.offsetLeft - posX2 <= 0 && items.offsetLeft - posX2 >= difference) {
-        items.style.left = (items.offsetLeft - posX2) + "px";
-    }
-  }
-  
-  function dragEnd (e: any) {
-    posFinal = items.offsetLeft;
-    if (posFinal - posInitial < -threshold) { } else if (posFinal - posInitial > threshold) {
-
-    } else {
-      items.style.left = (posInitial) + "px";
-    }
-
-    document.onmouseup = null;
-    document.onmousemove = null;
+  init() {
+    window.addEventListener('mousemove', this.onMouseMove.bind(this));
+    this.tick()
   }
 
+  onMouseMove(e: MouseEvent) {
+    const point = {
+      x: e.clientX / window.innerWidth,
+      y: e.clientY / window.innerHeight
+    };
+
+    this.waterTexture.addPoint(point)
+  }
+
+  tick() {
+    this.waterTexture.update();
+    requestAnimationFrame(this.tick)
+  }
 }
 
-slide(slider, sliderItems);
+
+const myApp = new App()
